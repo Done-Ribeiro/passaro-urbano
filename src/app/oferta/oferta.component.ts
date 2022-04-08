775aaa909f0559a367fe974d8971d2bf53fbbca5
Done Ribeiro <www.done_@hotmail.com>
2022-04-08 17:33:19 -0300

    subscribe - retrieving route parameters

--- src/app/oferta/oferta.component.ts
+++ src/app/oferta/oferta.component.ts
@@ -11,7 +11,9 @@ export class OfertaComponent implements OnInit {
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    console.log('ID recuperado da rota: ', this.route.snapshot.params['id'])
+    this.route.params.subscribe((parametro: any) => {
+      console.log(parametro.id)
+    })
   }
 
 }
