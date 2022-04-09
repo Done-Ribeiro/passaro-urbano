5df5e78030593ca97ad45264d6ea48487ad81a72
Done Ribeiro <www.done_@hotmail.com>
2022-04-09 18:35:23 -0300

    adjusting API access url

--- src/app/ofertas.service.ts
+++ src/app/ofertas.service.ts
@@ -1,27 +1,30 @@
 import { HttpClient } from '@angular/common/http'
 import { Injectable } from '@angular/core'
 import { Oferta } from "./shared/oferta.model"
+import { URL_API } from './app.api'
 
 @Injectable()
 export class OfertasService {
+
+  // private url_api = 'http://localhost:3000/ofertas'
 
   constructor(private http: HttpClient) {}
 
   public getOfertas(): Promise<Oferta[]> {
     //efetuar uma requisição http
-    return this.http.get('http://localhost:3000/ofertas?destaque=true')//retorna um observable, e por enquanto.. vamos converter para Promise
+    return this.http.get(`${URL_API}?destaque=true`)//retorna um observable, e por enquanto.. vamos converter para Promise
       .toPromise()//converte Observable para Promise
       .then((resposta: any) => resposta)//retornar uma promessa, contendo um Oferta[]
   }
 
   public getOfertasPorCategoria(categoria: string): Promise<Oferta[]> {
-    return this.http.get(`http://localhost:3000/ofertas?categoria=${categoria}`)
+    return this.http.get(`${URL_API}?categoria=${categoria}`)
       .toPromise()
       .then((resposta: any) => resposta)
   }
 
   public getOfertaPorId(id: number): Promise<Oferta> {
-    return this.http.get(`http://localhost:3000/ofertas?id=${id}`)
+    return this.http.get(`${URL_API}?id=${id}`)
       .toPromise()
       .then((resposta: any) => {
         return resposta[0]//retorna a oferta, e nao um array de ofertas
