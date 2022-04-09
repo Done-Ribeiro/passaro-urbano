8cb99857f2ebd90b6ad3a3cb1acfacc3878cee87
Done Ribeiro <www.done_@hotmail.com>
2022-04-09 20:24:54 -0300

    retrieving "como-usar" data via HTTP Request

--- src/app/ofertas.service.ts
+++ src/app/ofertas.service.ts
@@ -10,23 +10,29 @@ export class OfertasService {
 
   public getOfertas(): Promise<Oferta[]> {
     //efetuar uma requisição http
-    return this.http.get(`${URL_API}?destaque=true`)//retorna um observable, e por enquanto.. vamos converter para Promise
+    return this.http.get(`${URL_API}/ofertas?destaque=true`)//retorna um observable, e por enquanto.. vamos converter para Promise
       .toPromise()//converte Observable para Promise
       .then((resposta: any) => resposta)//retornar uma promessa, contendo um Oferta[]
   }
 
   public getOfertasPorCategoria(categoria: string): Promise<Oferta[]> {
-    return this.http.get(`${URL_API}?categoria=${categoria}`)
+    return this.http.get(`${URL_API}/ofertas?categoria=${categoria}`)
       .toPromise()
       .then((resposta: any) => resposta)
   }
 
   public getOfertaPorId(id: number): Promise<Oferta> {
-    return this.http.get(`${URL_API}?id=${id}`)
+    return this.http.get(`${URL_API}/ofertas?id=${id}`)
       .toPromise()
       .then((resposta: any) => {
         return resposta[0]//retorna a oferta, e nao um array de ofertas
       })
+  }
+
+  public getComoUsarOfertaPorId(id: number): Promise<string> {
+    return this.http.get(`${URL_API}/como-usar?id=${id}`)
+      .toPromise()
+      .then((resposta: any) => resposta[0].descricao)
   }
 
 }
