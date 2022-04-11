dffa5636ed67d4c8ab4838e23ac8acc50af96334
Done Ribeiro <www.done_@hotmail.com>
2022-04-11 14:44:13 -0300

    clean examples

--- src/app/oferta/oferta.component.ts
+++ src/app/oferta/oferta.component.ts
@@ -3,12 +3,6 @@ import { ActivatedRoute } from '@angular/router'
 import { OfertasService } from '../ofertas.service'
 import { Oferta } from '../shared/oferta.model'
 
-import { Observable } from 'rxjs'
-import { interval } from 'rxjs'
-import { Observer } from 'rxjs'
-
-import { Subscription } from 'rxjs'
-
 @Component({
   selector: 'app-oferta',
   templateUrl: './oferta.component.html',
@@ -16,9 +10,6 @@ import { Subscription } from 'rxjs'
   providers: [ OfertasService ]
 })
 export class OfertaComponent implements OnInit, OnDestroy {
-
-  private tempoObservableSubscription!: Subscription
-  private meuObservableTesteSubscription!: Subscription
 
   public oferta!: Oferta
 
@@ -30,32 +21,9 @@ export class OfertaComponent implements OnInit, OnDestroy {
   ngOnInit(): void {
     this.ofertasService.getOfertaPorId(this.route.snapshot.params['id'])
       .then(( oferta: Oferta ) => this.oferta = oferta)
-
-
-    let tempo = interval(2000)
-    this.tempoObservableSubscription = tempo.subscribe((intervalo: number) => {
-      console.log(intervalo)
-    })
-
-    //observable (observável)
-    let meuObservableTeste = Observable.create((observer: Observer<number>) => {
-      observer.next(1)
-      observer.next(3)
-      // observer.error('algum erro foi encontrado na strem de eventos')
-      observer.complete()
-    })
-
-    //observable (observador)
-    this.meuObservableTesteSubscription = meuObservableTeste.subscribe(
-      (resultado: number) => console.log(resultado + 10),
-      (erro: string) => console.log(erro),
-      () => console.log('Stream de eventos foi finalizada')
-    )
   }
 
   ngOnDestroy(): void {
-    this.meuObservableTesteSubscription.unsubscribe()
-    this.tempoObservableSubscription.unsubscribe()
   }
 
 }
