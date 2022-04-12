bd2f2633216c88119ac9b23b00925fffa4a632ba
Done Ribeiro <www.done_@hotmail.com>
2022-04-12 20:02:53 -0300

    Updating offer in route change process

--- src/app/oferta/oferta.component.ts
+++ src/app/oferta/oferta.component.ts
@@ -3,6 +3,8 @@ import { ActivatedRoute } from '@angular/router'
 import { OfertasService } from '../ofertas.service'
 import { Oferta } from '../shared/oferta.model'
 
+import { Params } from '@angular/router'
+
 @Component({
   selector: 'app-oferta',
   templateUrl: './oferta.component.html',
@@ -19,8 +21,19 @@ export class OfertaComponent implements OnInit, OnDestroy {
   ) { }
 
   ngOnInit(): void {
-    this.ofertasService.getOfertaPorId(this.route.snapshot.params['id'])
-      .then(( oferta: Oferta ) => this.oferta = oferta)
+    /**
+     * ! fazer subscribe nos parametros da rota
+     * * agora vamos combinar um subscribe com um observable
+     * para que, a cada evento de modificação de parametros da rota
+     * execute uma Promise
+     * * e vamos encaminhar pra essa promise, o id atualizado da rota
+     * ? (porque podemos combinar, observables com promises)
+    */
+    this.route.params.subscribe((parametros: Params) => {//fica escutando... quando algum parametro mudar, (no caso o id da rota)
+      this.ofertasService.getOfertaPorId(parametros.id)//passa o novo parametro (id)
+        .then(( oferta: Oferta ) => this.oferta = oferta)//e resolve a promise, atualizando o conteudo de this.oferta
+    })//volta a ficar escutando, porque eh uma promise sendo resolvida dentro de um observable
+
   }
 
   ngOnDestroy(): void {
