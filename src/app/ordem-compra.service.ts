8d5bc9e1c3dcb11cec66d14d2a722ad24fbff549
Done Ribeiro <www.done_@hotmail.com>
2022-04-17 10:58:02 -0300

    Injecting Angular 4 HTTP Service into OrderOrderService

--- src/app/ordem-compra.service.ts
+++ src/app/ordem-compra.service.ts
@@ -1,6 +1,12 @@
 import { Pedido } from "./shared/pedido.model"
+import { Injectable } from "@angular/core"
+import { HttpClient } from "@angular/common/http"
 
+@Injectable()
 export class OrdemCompraService {
+
+  constructor(private http: HttpClient) { }
+
   public efetivarCompra(pedido: Pedido): void {
     console.log(pedido)
   }
