612eac543fa9c1a91c62186256229cbd1d8b9b0c
Done Ribeiro <www.done_@hotmail.com>
2022-04-18 15:12:22 -0300

    Submitting the form via HTTP request (POST) to API Rest

--- src/app/ordem-compra.service.ts
+++ src/app/ordem-compra.service.ts
@@ -2,12 +2,28 @@ import { Pedido } from "./shared/pedido.model"
 import { Injectable } from "@angular/core"
 import { HttpClient } from "@angular/common/http"
 
+import { Observable } from "rxjs"
+import { URL_API } from "./app.api"
+import { HttpHeaders } from "@angular/common/http"
+import { map } from "rxjs/operators"
+
 @Injectable()
 export class OrdemCompraService {
 
   constructor(private http: HttpClient) { }
 
-  public efetivarCompra(pedido: Pedido): void {
-    console.log(pedido)
+  public efetivarCompra(pedido: Pedido): Observable<any> {
+    let headers = new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+    let options = { headers }
+
+    return this.http.post(
+      `${URL_API}/pedidos`,
+      JSON.stringify(pedido),
+      options
+    ).pipe(
+      map((resposta: any) => console.log(resposta))
+    )
   }
 }
--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -113,6 +113,7 @@ export class OrdemCompraComponent implements OnInit {
     this.pedido.formaPagamento = this.formaPagamento
 
     this.ordemCompraService.efetivarCompra(this.pedido)
+      .subscribe()
   }
 
 }
