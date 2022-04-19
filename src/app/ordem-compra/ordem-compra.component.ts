3d8a5638de3f57cb83a880783a7a462d22c56061
Done Ribeiro <www.done_@hotmail.com>
2022-04-19 15:11:16 -0300

    Adjusting things before starting with Reactive Forms

--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -1,8 +1,6 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { OrdemCompraService } from '../ordem-compra.service'
 import { Pedido } from '../shared/pedido.model'
-
-import { NgForm } from '@angular/forms'
 
 @Component({
   selector: 'app-ordem-compra',
@@ -12,28 +10,12 @@ import { NgForm } from '@angular/forms'
 })
 export class OrdemCompraComponent implements OnInit {
 
-  @ViewChild('formulario') public formulario!: NgForm
-
-  public idPedidoCompra!: number
-
   constructor(private ordemCompraService: OrdemCompraService) { }
 
   ngOnInit() {
-
+    
   }
 
   public confirmarCompra(): void {
-    let pedido: Pedido = new Pedido(
-      this.formulario.value.endereco,
-      this.formulario.value.numero,
-      this.formulario.value.complemento,
-      this.formulario.value.formaPagamento,
-    )
-
-    this.ordemCompraService.efetivarCompra(pedido)
-      .subscribe((idPedido: number) => {
-        this.idPedidoCompra = idPedido
-      })
   }
-
 }
